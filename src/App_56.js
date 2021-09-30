5857ef5e908014f5a3e0b37756af73467d90079f
209410256 <209410256@gms.tku.edu.tw>
2021-09-30 19:54:44 +0800

    first commit

---
+++ src/App_56.js
@@ -0,0 +1,15 @@
+import Todo_56 from './components/Todo_56';
+import './App.css';
+
+function App_56() {
+  return (
+    <div className="App">
+      <h1>My Todos</h1>
+      <Todo_56 text="Learn React" />
+      <Todo_56 text="Learn SQL" />
+      <Todo_56 text="Learn Next js" />
+    </div>
+  );
+}
+
+export default App_56;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App_56 from './App_56';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App_56 />
   </React.StrictMode>,
   document.getElementById('root')
 );
